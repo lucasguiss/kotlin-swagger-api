452b518e94cbc5b93abaf2d0902f3f64f99ab6e7
Lucas Guiss Gusmão <lucasguissgusmao@gmail.com>
2021-10-03 16:21:11 -0300

    chore: creating base project

---
+++ src/main/kotlin/br/com/lucasguiss/kotlinswaggerapi/KotlinSwaggerApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.lucasguiss.kotlinswaggerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSwaggerApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSwaggerApiApplication>(*args)
+}
