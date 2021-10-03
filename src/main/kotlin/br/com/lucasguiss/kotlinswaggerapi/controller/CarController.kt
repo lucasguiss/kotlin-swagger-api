162f32aceca264d133333a0452e4fdac9e23d8bb
Lucas Guiss Gusmão <lucasguissgusmao@gmail.com>
2021-10-03 18:42:23 -0300

    feat(swagger): documenting car controller and model

--- src/main/kotlin/br/com/lucasguiss/kotlinswaggerapi/controller/CarController.kt
+++ src/main/kotlin/br/com/lucasguiss/kotlinswaggerapi/controller/CarController.kt
@@ -2,17 +2,26 @@ package br.com.lucasguiss.kotlinswaggerapi.controller
 
 import br.com.lucasguiss.kotlinswaggerapi.model.Car
 import br.com.lucasguiss.kotlinswaggerapi.service.CarService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
+@Api("Car API")
 @RestController
 @RequestMapping(path = ["car"])
 class CarController(
     private val service: CarService
 ) {
 
+    @ApiOperation(
+        value = "Create a car",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        code = 201
+    )
     @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
@@ -26,6 +35,11 @@ class CarController(
         }
     }
 
+    @ApiOperation(
+        value = "Find all cars",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        code = 200
+    )
     @GetMapping(
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
--- src/main/kotlin/br/com/lucasguiss/kotlinswaggerapi/model/Car.kt
+++ src/main/kotlin/br/com/lucasguiss/kotlinswaggerapi/model/Car.kt
@@ -1,8 +1,14 @@
 package br.com.lucasguiss.kotlinswaggerapi.model
 
+import io.swagger.annotations.ApiModelProperty
+
 data class Car(
+    @ApiModelProperty("Identifier of the car", required = true, example = "1")
     val id: Long,
+    @ApiModelProperty("Model of the", required = true, example = "Sandero")
     val model: String,
+    @ApiModelProperty("Year of the car", required = true, example = "2009")
     val year: Int,
+    @ApiModelProperty("Brand name of the car", required = true, example = "Renault")
     val brand: String
 )
